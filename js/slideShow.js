c4a68557177d48457b1698d79468a2826631f997
humanrizki <humanrizki123@gmail.com>
2021-10-24 19:53:11 +0700

    membuat postingan blog di home

--- js/slideShow.js
+++ js/slideShow.js
@@ -1,7 +1,40 @@
 const slides = document.querySelectorAll('.slides-img');
 let indexImg = 0;
 slides[0].classList.toggle('aktif');
-let buttons = document.querySelector('.button').querySelectorAll('button').forEach((button,indexB)=>{
+slides.forEach((img)=>{
+    if(img.classList.contains('aktif')){
+        img.querySelector('img').addEventListener('mouseover',(e)=>{
+            document.querySelectorAll('.buttons').forEach((btn)=>{
+                btn.classList.add('minggir');
+                btn.style.transition = 'all .5s ease';
+            });
+        });
+        img.querySelector('img').addEventListener('mouseout',(e)=>{
+            document.querySelectorAll('.buttons').forEach((btn)=>{
+                btn.classList.remove('minggir');
+                btn.style.transition = 'all .5s ease';
+            });
+        });
+        document.querySelector('.button').addEventListener('mouseover',(button)=>{
+            button.target.querySelectorAll('.buttons').forEach((buttons)=>{
+                buttons.classList.add('minggir');
+                buttons.addEventListener('mouseover',(btn)=>{
+                    btn.target.classList.add('minggir');
+                    
+                });
+            });
+        });
+        document.querySelector('.button').addEventListener('mouseout',(button)=>{
+            button.target.querySelectorAll('.buttons').forEach((buttons)=>{
+                buttons.classList.toggle('minggir');
+                
+            });
+        });
+            
+    }
+});
+
+let buttons = document.querySelector('.button').querySelectorAll('.buttons').forEach((button,indexB)=>{
     if(button.classList.contains('button-left')){
         button.addEventListener('click',()=>{
             nextImage(-1,'prev');
@@ -28,5 +61,67 @@ let buttons = document.querySelector('.button').querySelectorAll('button').forEa
             slides[i].classList.remove('aktif');
         }
         slides[indexImg].classList.add('aktif');
+        // slides.forEach((img)=>{
+        //     if(img.classList.contains('aktif')){
+        //         img.querySelector('img').addEventListener('mouseover',(e)=>{
+        //             document.querySelectorAll('.buttons').forEach((btn)=>{
+        //                 btn.style.margin = "0px 20px";
+        //                 btn.style.transition = 'all .5s ease';
+        //             });
+        //         });
+        //         img.querySelector('img').addEventListener('mouseout',(e)=>{
+        //             document.querySelectorAll('.buttons').forEach((btn)=>{
+        //                 btn.style.margin = "0px -20px";
+        //                 btn.style.transition = 'all .5s ease';
+        //             });
+        //         });
+        //     }
+        // });
+        // slides.addEventListener('mouseover',()=>{
+        //     document.querySelectorAll('.buttons').forEach((btn)=>{
+        //         btn.style.margin = "0px 20px";
+        //         btn.style.transition = 'all .5s ease';
+        //     });
+        // });
+        // slides.addEventListener('mouseout',()=>{
+        //     document.querySelectorAll('.buttons').forEach((btn)=>{
+        //         btn.style.margin = "0px -20px";
+        //         btn.style.transition = 'all .5s ease';
+        //     });
+        // });
+        slides.forEach((img)=>{
+            if(img.classList.contains('aktif')){
+                img.querySelector('img').addEventListener('mouseover',(e)=>{
+                    document.querySelectorAll('.buttons').forEach((btn)=>{
+                        btn.classList.add('minggir');
+                        btn.style.transition = 'all .5s ease';
+                    });
+                });
+                img.querySelector('img').addEventListener('mouseout',(e)=>{
+                    document.querySelectorAll('.buttons').forEach((btn)=>{
+                        btn.classList.remove('minggir');
+                        btn.style.transition = 'all .5s ease';
+                    });
+                });
+                document.querySelector('.button').addEventListener('mouseover',(button)=>{
+                    button.target.querySelectorAll('.buttons').forEach((buttons)=>{
+                        buttons.classList.add('minggir');
+                        buttons.addEventListener('mouseover',(btn)=>{
+                            btn.target.classList.add('minggir');
+                            
+                        });
+                    });
+                });
+                document.querySelector('.button').addEventListener('mouseout',(button)=>{
+                    button.target.querySelectorAll('.buttons').forEach((buttons)=>{
+                        buttons.classList.toggle('minggir');
+                        
+                    });
+                });
+                    
+            }
+        });
+        
     };
+    
 })
